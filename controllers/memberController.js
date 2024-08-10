d8c05f29903f757419715797125900c826c82290
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-08-10 18:32:41 +0530

    Update memberController.js

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,1 +1,28 @@
 
+const Member = require('../models/memberModel'); // Adjust the path based on your project structure
+
+// Insert multiple members
+exports.insertMembers = async (req, res) => {
+    try {
+        const data = req.body; // Assuming req.body contains an array of member objects
+
+        if (Array.isArray(data)) {
+            const result = await Member.insertMany(data);
+            console.log(result)
+            res.status(201).json({
+                message: 'Families added successfully',
+                data: result
+            });
+        } else {
+            const result = await Member.create(data);
+            console.log(result)
+            res.status(201).json({
+                message: 'Family added successfully',
+                data: result
+            });
+        }
+    } catch (error) {
+        console.error('Error inserting members:', error);
+        res.status(500).json({ success: false, message: 'Error inserting members' });
+    }
+};
