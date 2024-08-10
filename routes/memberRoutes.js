68a000413febe7aa65b09e2429c6063a343f2b37
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-08-10 18:31:30 +0530

    Update memberRoutes.js

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { insertMembers } = require('../controllers/memberController.js');
 
+// Route to handle insertion of multiple members
+router.post('/members', insertMembers);
+
+module.exports = router;
